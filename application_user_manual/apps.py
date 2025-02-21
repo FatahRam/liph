aeae6031d53bf33309f4b21f8c07514cf9d1cac6
FatahRam <64979016+FatahRam@users.noreply.github.com>
2025-02-21 01:40:25 +0100

    Add files via upload

---
+++ application_user_manual/apps.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2f7521a735ec095e19244600935b52a06cb3bcb748cc77a762b3ae7732db2284
+size 176
---
+++ application_user_manual/views.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:58e1cd41190113b3a392289f5662e8bf4db42ebc55f62445900662231c315105
+size 788
