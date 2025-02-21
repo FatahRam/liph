a469e8d812cfe2263eff6a3e225b0070a78a9621
FatahRam <64979016+FatahRam@users.noreply.github.com>
2025-02-21 02:03:07 +0100

    Add files via upload

---
+++ visite_duo/apps.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d6ee063e87969ec618b10959bb8bcbfc2a6a5ea3153245e587bdcf45e9eb84b1
+size 151
---
+++ visite_duo/views.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a450147beaecf1f5314ca67e50b88acb01474cbf6c3fca2194a56affa80b153d
+size 1074
